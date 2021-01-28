553d804294940701d81ed4124dfe3453d8b421ef
王鑫龙 <7464772+xinlongwang@user.noreply.gitee.com>
2021-01-28 15:45:48 +0800

    初始化8888

--- src/main/java/com/wangpip/jenkins/controller/JenkinsController.java
+++ src/main/java/com/wangpip/jenkins/controller/JenkinsController.java
@@ -22,6 +22,6 @@ public class JenkinsController {
 
     @GetMapping("/index")
     public Object index(){
-        return "hello jenkins 123456 ";
+        return "hello jenkins 123456 2222";
     }
 }
