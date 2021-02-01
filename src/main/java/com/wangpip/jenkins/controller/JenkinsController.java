a5cd22097e278a3c5d0ef591c030c7962cddb8b5
王鑫龙 <7464772+xinlongwang@user.noreply.gitee.com>
2021-02-01 17:06:02 +0800

    初始化222000123456121

--- src/main/java/com/wangpip/jenkins/controller/JenkinsController.java
+++ src/main/java/com/wangpip/jenkins/controller/JenkinsController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class JenkinsController {
 
     private int a;
-   //
+   ////
     private int c;
 
     @GetMapping("/index")
