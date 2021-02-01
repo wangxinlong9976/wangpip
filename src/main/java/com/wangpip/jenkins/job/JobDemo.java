4eafb5a353ac6c90e2c04ce5d1c3bb70255e578d
王鑫龙 <7464772+xinlongwang@user.noreply.gitee.com>
2021-02-01 14:35:37 +0800

    初始化222000

---
+++ src/main/java/com/wangpip/jenkins/job/JobDemo.java
@@ -0,0 +1,21 @@
+package com.wangpip.jenkins.job;
+
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+/**
+ * ClassName: JobDemo <br/>
+ * Description: <br/>
+ * date: 2021/1/28 16:49<br/>
+ *
+ * @author lenovo<br />
+ * @version v1.0
+ * @since JDK 1.8
+ */
+public class JobDemo extends QuartzJobBean {
+    @Override
+    protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        System.out.println(jobExecutionContext.getJobDetail().getKey());
+    }
+}
